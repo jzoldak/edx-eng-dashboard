a055e931a25cdb8aca8bbdca679d64057fa14bf4
Jay Zoldak <jzoldak@edx.org>
2013-03-27 15:59:03 -0400

    Send mitx version to a list so we can report on multiple servers

--- jobs/mitx_hash.rb
+++ jobs/mitx_hash.rb
@@ -3,11 +3,12 @@ require "json"
 
 host = 'prod-edx-001.m.edx.org'
 port = '8099'
+request_uri = '/versions.json'
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
   http = Net::HTTP.new(host, port)
-  response = http.request(Net::HTTP::Get.new('/versions.json'))
+  response = http.request(Net::HTTP::Get.new(request_uri))
   mitx_hash = JSON.parse(response.body)['mitx']
-  send_event('mitx_hash', { mitx: mitx_hash })
+  send_event('mitx_hash_list', { title: 'mitx version', unordered: true, items: [label: host, value: mitx_hash] })
 end
--- widgets/mitx_version/mitx_version.coffee
+++ widgets/mitx_version/mitx_version.coffee
@@ -1,9 +1,7 @@
 class Dashing.MitxVersion extends Dashing.Widget
-
   ready: ->
     # This is fired when the widget is done being rendered
-
-  onData: (data) ->
-    # Handle incoming data
-    # You can access the html node of this widget with `@node`
-    # Example: $(@node).fadeOut().fadeIn() will make the node flash each time data comes in.
+    if @get('unordered')
+      $(@node).find('ol').remove()
+    else
+      $(@node).find('ul').remove()

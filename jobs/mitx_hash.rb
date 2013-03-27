09f480c9fdf04fe740f147c2c5d2fa88b43a98a7
Jay Zoldak <jzoldak@edx.org>
2013-03-27 15:22:14 -0400

    Start putting hash on the dashboard

--- jobs/mitx_hash.rb
+++ jobs/mitx_hash.rb
@@ -1,4 +1,4 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('widget_id', { })
+  send_event('mitx_hash', { mitx: "c70f4eea" })
 end

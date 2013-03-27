ac8f2e31dea085ec5da7c985ea5e55f9abd21466
Jay Zoldak <jzoldak@edx.org>
2013-03-27 15:34:27 -0400

    Get mitx hash onto the dashboard

--- jobs/mitx_hash.rb
+++ jobs/mitx_hash.rb
@@ -1,4 +1,13 @@
+require "net/http"
+require "json"
+
+host = 'prod-edx-001.m.edx.org'
+port = '8099'
+
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('mitx_hash', { mitx: "c70f4eea" })
+  http = Net::HTTP.new(host, port)
+  response = http.request(Net::HTTP::Get.new('/versions.json'))
+  mitx_hash = JSON.parse(response.body)['mitx']
+  send_event('mitx_hash', { mitx: mitx_hash })
 end

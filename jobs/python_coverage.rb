8e36bdeac2218cc7d06aa6f5235d33ee500fac61
Jay Zoldak <jzoldak@edx.org>
2013-03-28 11:11:37 -0400

    Represent coverage with meters

--- jobs/python_coverage.rb
+++ jobs/python_coverage.rb
@@ -11,7 +11,7 @@ systems = ['cms', 'lms', 'common/lib/capa', 'common/lib/xmodule']
 pc_cov = {}
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '1m', :first_in => 0 do |job|
+SCHEDULER.every '5m', :first_in => 0 do |job|
     systems.each do |system|
         request_uri = uri_base + system + '/cover/'
         response = http.request(Net::HTTP::Get.new(request_uri))
@@ -19,9 +19,9 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
         pc_cov[system] = doc.at_css("span.pc_cov").text
     end
 
-    send_event('mitx_py_coverage', { title: 'Python Coverage', unordered: true, 
-        items: [{label: 'CMS', value: pc_cov['cms']}, 
-                {label: 'LMS', value: pc_cov['lms']},
-                {label: 'capa', value: pc_cov['common/lib/capa']},
-                {label: 'xmodule', value: pc_cov['common/lib/xmodule']}] })
+    send_event('py_cms',   { value: pc_cov['cms'].delete('%').to_i })
+    send_event('py_lms',   { value: pc_cov['lms'].delete('%').to_i })
+    send_event('py_capa',   { value: pc_cov['common/lib/capa'].delete('%').to_i })
+    send_event('py_xmodule',   { value: pc_cov['common/lib/xmodule'].delete('%').to_i })
+
 end

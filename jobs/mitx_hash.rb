49284240ee516cbae675ec320b1071d8ef45c6fd
Jay Zoldak <jzoldak@edx.org>
2013-04-04 11:16:28 -0400

    Show mitx versions for all stage and prod servers

--- jobs/mitx_hash.rb
+++ jobs/mitx_hash.rb
@@ -1,14 +1,43 @@
 require "net/http"
 require "json"
 
-host = 'prod-edx-001.m.edx.org'
 port = '8099'
 request_uri = '/versions.json'
 
+p_hosts = ["prod-edge-edxapp-001",
+           "prod-edge-edxapp-002",
+           "prod-edx-001",
+           "prod-edx-002",
+           "prod-edx-003",
+           "prod-edxapp-001",
+           "prod-edxapp-002",
+           "prod-edxapp-003"
+           ]
+s_hosts = ["stage-edge-edxapp-001",
+           "stage-edge-edxapp-002",
+           "stage-edxapp-001",
+           "stage-edxapp-002"
+           ]
+
+suffix = '.m.edx.org'
+
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  http = Net::HTTP.new(host, port)
-  response = http.request(Net::HTTP::Get.new(request_uri))
-  mitx_hash = JSON.parse(response.body)['mitx']
-  send_event('mitx_hash_list', { title: 'mitx hash', unordered: true, items: [label: host, value: mitx_hash] })
+    prod_hash = Hash.new({ value: "N/A" })
+    p_hosts.each do |host|
+        http = Net::HTTP.new(host + suffix, port)
+        response = http.request(Net::HTTP::Get.new(request_uri))
+        mitx_hash = JSON.parse(response.body)['mitx']
+        prod_hash[host] =  { label: host, value: mitx_hash }
+    end
+    stage_hash = Hash.new({ value: "N/A" })
+    s_hosts.each do |host|
+        http = Net::HTTP.new(host + suffix, port)
+        response = http.request(Net::HTTP::Get.new(request_uri))
+        mitx_hash = JSON.parse(response.body)['mitx']
+        stage_hash[host] = { label: host, value: mitx_hash }
+    end
+
+    send_event('mitx_hash_prod', { title: 'Production Version', unordered: true, items: prod_hash.values })
+    send_event('mitx_hash_stage', { title: 'Staging Version', unordered: true, items: stage_hash.values })
 end

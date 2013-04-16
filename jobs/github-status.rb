037a0f4487c1b1a02f4e2c907b1ade489520f22f
Jay Zoldak <jzoldak@edx.org>
2013-04-16 11:34:13 -0400

    Catch timeouts in github status API

--- jobs/github-status.rb
+++ jobs/github-status.rb
@@ -5,9 +5,10 @@ require "uri"
 uri = URI.parse("https://status.github.com/api/status.json")
 widget = 'github_status'
 
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+# :first_in sets how long it takes before the job is first run.
+# In this case, it is run immediately.
 # TODO: rescue Timeout::Error if the back end does not respond
-SCHEDULER.every '1m', :first_in => 0 do |job|
+SCHEDULER.every '10s', :first_in => 0 do |job|
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -17,10 +18,12 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     response = http.request(request)
     b = response.body
     status = JSON.parse(b)['status']
+    updated = JSON.parse(b)['last_updated']
 
     # note: status warning=red and danger=orange
     if response.code != '200'
-        send_event(widget, { title: 'GitHub', text: response.code, status: 'danger', moreinfo: 'response code is not 200' })
+        send_event(widget, { title: 'GitHub', text: response.code,
+            status: 'danger', moreinfo: 'response code is not 200' })
     else
         if status.nil?
             code = 'danger'
@@ -33,7 +36,10 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
         else
             code == 'danger'
         end
-        moreinfo = 'DEBUG: color = ' + code
-        send_event(widget, { text: status, status: code, moreinfo: moreinfo })
+        send_event(widget, { text: status, status: code, moreinfo: updated })
     end
+
+    rescue Timeout::Error
+        send_event(widget, { title: 'GitHub', text: 'Timeout',
+            status: 'danger', moreinfo: 'timeout exception caught' })
 end

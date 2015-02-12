c65d511907cb04801871702d6957aa5289514696
Ben Patterson <bpatterson@edx.org>
2015-02-12 16:56:18 -0500

    Initial buildout for analytics coverage.

--- config/settings.rb
+++ config/settings.rb
@@ -1,3 +1,7 @@
 JENKINS_BASE_URL = 'https://build.testeng.edx.org'
 JENKINS_JOB_NAME = 'edx-platform-all-tests-master-flow'
 JENKINS_JOB_URL = JENKINS_BASE_URL + '/job/' + JENKINS_JOB_NAME
+
+COVERALLS_URL = 'https://coveralls.io/r/edx/'
+
+ANALYTICS_METER_COLOR="#CC0099"

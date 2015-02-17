77bf4d5acd7e1f2a2dbbf96203ef44d880bb60d4
Ben Patterson <bpatterson@edx.org>
2015-02-17 07:20:16 -0500

    Remove old Jenkins references.

--- jobs/edx_jenkins.rb
+++ jobs/edx_jenkins.rb
@@ -6,7 +6,7 @@ SCHEDULER.every '60s', :first_in => 0 do
     current_status = nil
     last_status = current_status
 
-    build_uri = OLD_JENKINS_JOB_URL + "/lastBuild/api/json"
+    build_uri = JENKINS_JOB_URL + "/lastBuild/api/json"
 
     # Once SSL certificates are set up correctly, we can remove the :ssl_verify_mode option
     data = open(build_uri, :ssl_verify_mode=>OpenSSL::SSL::VERIFY_NONE).read
@@ -16,30 +16,7 @@ SCHEDULER.every '60s', :first_in => 0 do
     if build_info["building"] == true
         current_status = "BUILDING"
     end
-    send_event("old_jenkins_status", {
-        resultText: current_status,
-        fullDisplayName: build_info["fullDisplayName"],
-        currentResult: current_status,
-        lastResult: last_status,
-        number: build_info["number"],
-        url: build_info["url"],
-        timestamp: build_info["timestamp"] })
-
-
-    current_status = nil
-    last_status = current_status
-
-    build_uri = NEW_JENKINS_JOB_URL + "/lastBuild/api/json"
-
-    # Once SSL certificates are set up correctly, we can remove the :ssl_verify_mode option
-    data = open(build_uri, :ssl_verify_mode=>OpenSSL::SSL::VERIFY_NONE).read
-
-    build_info = JSON.parse(data)
-    current_status = build_info["result"]
-    if build_info["building"] == true
-        current_status = "BUILDING"
-    end
-    send_event("new_jenkins_status", {
+    send_event("jenkins_status", {
         resultText: current_status,
         fullDisplayName: build_info["fullDisplayName"],
         currentResult: current_status,
